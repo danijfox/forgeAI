5c09edd6af6859713b0f3af914764d43740d76cf
Daniel Paino <danielpainoserrano@gmail.com>
2025-10-08 09:06:16 +0000

    fix(auth): use default firebaseapp domain for auth callbacks

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,8 @@ import { getStorage, FirebaseStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  // Use the default Firebase auth domain to handle auth callbacks correctly.
+  authDomain: "studio-774665260-80d2e.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
